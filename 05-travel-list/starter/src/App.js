b82cf610793145486888b5bf69138f7a99b9872e
SuaDeDua <sokvai123hn@gmail.com>
2024-06-06 15:16:35 +0700

    first commit

---
+++ 05-travel-list/starter/src/App.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import PackingList from "./PackingList";
+import Form from "./Form";
+import Logo from "./Logo";
+import Stats from "./Stats";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItem(newItem) {
+    setItems([...items, newItem]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItem={handleAddItem} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
