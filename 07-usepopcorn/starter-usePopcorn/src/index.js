de0213eef6697242621a71447fdbe8dc103ac360
SuaDeDua <sokvai123hn@gmail.com>
2024-06-26 17:02:49 +0700

    update something

--- 07-usepopcorn/starter-usePopcorn/src/index.js
+++ 07-usepopcorn/starter-usePopcorn/src/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-/* import './index.css';
-import App from './App'; */
+import './index.css';
+import App from './App';
 
 import StarRating from "./StarRating";
 
@@ -19,13 +19,13 @@ function Test() {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
       defaultRating={3}
     />
-    <StarRating maxRating={5} size={24} className="test" />
-    <Test />
+    <StarRating maxRating={5} size={24} className="test" /> */}
+    {/* <Test /> */}
   </React.StrictMode>
 );
